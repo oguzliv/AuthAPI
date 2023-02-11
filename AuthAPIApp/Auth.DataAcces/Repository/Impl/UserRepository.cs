7d1412fa274c7c04e4a2191aeba3e5f3a5cc2d4c
oguzliv <oguz.liv@renticar.com>
2023-02-11 13:07:12 +0300

    fix

--- AuthAPIApp/Auth.DataAcces/Repository/Impl/UserRepository.cs
+++ AuthAPIApp/Auth.DataAcces/Repository/Impl/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Auth.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace Auth.DataAcces.Repository.Impl
 {
-    internal class UserRepository
+    public class UserRepository : IUserRepository
     {
+        public Task<User> Create(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetById(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetOnlineUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
